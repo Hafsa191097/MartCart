4d30cc9e7826e0fdb57d09793ba7719616807760
Hafsa191097 <hafsaayyhere@gmail.com>
2023-10-16 01:39:09 -0700

    Now

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const authRouter = require('./Routes/AuthRouter');
 const mongoose = require('mongoose');
 
-const db = "mongodb+srv://hafsa:Hafsa_Here123@cluster0.oenrex9.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
 const app = express();
 app.use(express.json());
 app.use(authRouter);
